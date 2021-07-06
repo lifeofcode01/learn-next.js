1ec00b15398dedf6430bb402f9c052a05eb34799
Satyam <awarenett@gmail.com>
2021-07-06 15:44:01 +0530

    theme changes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,23 +1,16 @@
+// pages/_document.js
+
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import theme from "../shared/theme";
 
 export default class Document extends NextDocument {
-  static getInitialProps(ctx: DocumentContext) {
-    return NextDocument.getInitialProps(ctx);
-  }
-
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head />
         <body>
-          <ColorModeScript />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
